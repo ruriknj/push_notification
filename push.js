f48912e5457521446bb7b10a3fa34e2e0a6cc883
markjameshoward <markjameshoward@hotmail.co.uk>
2019-10-06 12:15:30 +1300

    fix

--- push.js
+++ push.js
@@ -12,6 +12,15 @@ push.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    'https://fcm.googleapis.com/fcm/send/fwMG_ImcKYc:APA91bHtTnPUQ0tBehI7Kk1CGn4s3yhIKhJunhqiggl5XxbyVPmqxDkugwctQMzL2VDTrG_ypKwWqb1Djv-kqWbxpmsatPSjSAUldFENqCIZShaFvZFiCwH-X2hS0-Xjqv5mq2XYHcf9',
+  expirationTime: null,
+  keys: {
+    p256dh:
+      'BLHrTAa9dZ4LV5DoCgtXCXLMd21_S3qyt4eSqn6UQCjIEAuemY0QI29Mvc-LL02e0DLGPKd3xkVHjCD4eqJWHSg',
+    auth: 'TW-LBZmYtXNW7VQUPX4I0w'
+  }
+};
 
 push.sendNotification(sub, 'test message');
